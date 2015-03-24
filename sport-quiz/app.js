15b726a22c6aa84d09193d78989d701147b41232
Milos Novicevic <milos@joiz.ch>
2015-03-24 02:13:26 +0100

    Added middlewares

--- sport-quiz/app.js
+++ sport-quiz/app.js
@@ -1,10 +1,7 @@
 var express = require('express'),
     path = require('path'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
     routes = require('./routes'),
+    middlewares = require('./middlewares'),
     app,
     server,
     io;
@@ -18,18 +15,8 @@ server = app.listen(3000);
 //our socket io logic is in separate file
 io = require('./sockets.js').listen(server)
 
-//do here list of all questions and crud logic as well
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app; //we are exporting app to other modules
---
+++ sport-quiz/middlewares/index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
+
+module.exports = function (app) {
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+};
